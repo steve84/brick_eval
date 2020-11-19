0127a9ac3cf4339e939be98769f8b502786c0c18
Steve Aschwanden <steve.aschwanden@bluewin.ch>
2020-11-19 21:01:19 +0100

    Change migration script (delete table contents except score)

--- api/db_migration.sql
+++ api/db_migration.sql
@@ -1,61 +1,113 @@
-INSERT OR REPLACE INTO colors (id, name, rgb, is_trans)
+-- Delete queries --
+PRAGMA foreign_keys = OFF;
+DELETE FROM colors WHERE id NOT NULL;
+DELETE FROM minifigs WHERE id NOT NULL;
+DELETE FROM part_categories WHERE id NOT NULL;
+DELETE FROM themes WHERE id NOT NULL;
+DELETE FROM sets WHERE id NOT NULL;
+DELETE FROM parts WHERE id NOT NULL;
+DELETE FROM elements WHERE element_id NOT NULL;
+DELETE FROM part_color_frequencies WHERE part_id NOT NULL;
+DELETE FROM inventories WHERE id NOT NULL;
+DELETE FROM part_relationships WHERE child_part_id NOT NULL;
+DELETE FROM inventory_minifigs WHERE inventory_id NOT NULL;
+DELETE FROM inventory_sets WHERE inventory_id NOT NULL;
+DELETE FROM inventory_parts WHERE inventory_id NOT NULL;
+DELETE FROM minifig_inventory_rel WHERE inventory_id NOT NULL;
+DELETE FROM set_inventory_rel WHERE inventory_id  NOT NULL;
+
+-- Insert queries --
+
+-- Base tables
+INSERT OR IGNORE INTO colors (id, name, rgb, is_trans)
 SELECT id, name, rgb, is_trans FROM colors_tmp;
 
-INSERT OR REPLACE INTO minifigs (fig_num, name, num_parts)
+INSERT OR IGNORE INTO minifigs (fig_num, name, num_parts)
 SELECT fig_num, name, num_parts FROM minifigs_tmp;
 
-INSERT OR REPLACE INTO part_categories (id, name)
+INSERT OR IGNORE INTO part_categories (id, name)
 SELECT id, name FROM part_categories_tmp;
 
-INSERT OR REPLACE INTO themes (id, name, parent_id)
+INSERT OR IGNORE INTO themes (id, name, parent_id)
 SELECT id, name, parent_id FROM themes_tmp;
 
-INSERT OR REPLACE INTO sets (set_num, name, year_of_publication, theme_id, num_parts)
+INSERT OR IGNORE INTO sets (set_num, name, year_of_publication, theme_id, num_parts)
 SELECT set_num, name, year_of_publication, theme_id, num_parts FROM sets_tmp;
 
-INSERT OR REPLACE INTO parts (part_num, name, part_cat_id, part_material)
+INSERT OR IGNORE INTO parts (part_num, name, part_cat_id, part_material)
 SELECT part_num, name, part_cat_id, part_material FROM parts_tmp;
 
-INSERT OR REPLACE INTO elements (element_id, part_id, color_id)
+INSERT OR IGNORE INTO elements (element_id, part_id, color_id)
 SELECT element_id, p.id, color_id FROM elements_tmp e
 left join parts p on e.part_num = p.part_num;
 
-
-INSERT OR REPLACE INTO part_color_frequencies (part_id, color_id)
-SELECT part_id, color_id FROM inventory_parts;
-
-INSERT OR REPLACE INTO inventories (id, set_id, version)
+INSERT OR IGNORE INTO inventories (id, set_id, version)
 SELECT t.id, s.id, version FROM inventories_tmp t
 left join sets s on t.set_num = s.set_num;
 
-INSERT OR REPLACE INTO part_relationships (rel_type, child_part_id, parent_part_id)
+INSERT OR IGNORE INTO part_relationships (rel_type, child_part_id, parent_part_id)
 SELECT rel_type, p1.id, p2.id FROM part_relationships_tmp t
 left join parts p1 on t.child_part_num = p1.part_num
 left join parts p2 on t.parent_part_num = p2.part_num;
 
-INSERT OR REPLACE INTO inventory_minifigs (inventory_id, fig_id, quantity)
+INSERT OR IGNORE INTO inventory_minifigs (inventory_id, fig_id, quantity)
 SELECT inventory_id, m.id, quantity FROM inventory_minifigs_tmp t
 left join minifigs m on m.fig_num = t.fig_num;
 
-INSERT OR REPLACE INTO inventory_sets (inventory_id, set_id, quantity)
+INSERT OR IGNORE INTO inventory_sets (inventory_id, set_id, quantity)
 SELECT inventory_id, s.id, quantity FROM inventory_sets_tmp t
 left join sets s on s.set_num = t.set_num;
 
-INSERT OR REPLACE INTO inventory_parts (inventory_id, part_id, color_id, is_spare, quantity)
+INSERT OR IGNORE INTO inventory_parts (inventory_id, part_id, color_id, is_spare, quantity)
 SELECT inventory_id, p.id, color_id, is_spare, quantity FROM inventory_parts_tmp t
 left join parts p on t.part_num = p.part_num;
 
+DELETE FROM scores WHERE inventory_id NOT IN (select id FROM inventories);
+PRAGMA foreign_keys = ON;
 
-INSERT OR REPLACE INTO minifig_inventory_rel (inventory_id, inventory_minifig_id)
+--- Insert check
+select c.amount = ct.amount from (select count(*) as amount from colors) c, (select count(*) as amount from colors_tmp) ct
+union all
+select c.amount = ct.amount from (select count(*) as amount from minifigs) c, (select count(*) as amount from minifigs_tmp) ct
+union all
+select c.amount = ct.amount from (select count(*) as amount from part_categories) c, (select count(*) as amount from part_categories_tmp) ct
+union all
+select c.amount = ct.amount from (select count(*) as amount from themes) c, (select count(*) as amount from themes_tmp) ct
+union all
+select c.amount = ct.amount from (select count(*) as amount from sets) c, (select count(*) as amount from sets_tmp) ct
+union all
+select c.amount = ct.amount from (select count(*) as amount from parts) c, (select count(*) as amount from parts_tmp) ct
+union all
+select c.amount = ct.amount from (select count(*) as amount from elements) c, (select count(*) as amount from elements_tmp) ct
+union all
+select c.amount = ct.amount from (select count(*) as amount from inventories) c, (select count(*) as amount from inventories_tmp) ct
+union all
+select c.amount = ct.amount from (select count(*) as amount from part_relationships) c, (select count(*) as amount from part_relationships_tmp) ct
+union all
+select c.amount = ct.amount from (select count(*) as amount from inventory_minifigs) c, (select count(*) as amount from inventory_minifigs_tmp) ct
+union all
+select c.amount = ct.amount from (select count(*) as amount from inventory_sets) c, (select count(*) as amount from inventory_sets_tmp) ct
+union all
+select c.amount = ct.amount from (select count(*) as amount from inventory_parts) c, (select count(*) as amount from inventory_parts_tmp) ct;
+
+
+-- Generated from base tables
+
+INSERT OR IGNORE INTO part_color_frequencies (part_id, color_id)
+SELECT part_id, color_id FROM inventory_parts;
+
+INSERT OR IGNORE INTO minifig_inventory_rel (inventory_id, inventory_minifig_id)
 SELECT i.id, im.id FROM inventory_minifigs im
 left join minifigs m on im.fig_id = m.id
 left join inventories_tmp i on m.fig_num = i.set_num;
 
-INSERT OR REPLACE INTO set_inventory_rel (inventory_id, inventory_set_id)
+INSERT OR IGNORE INTO set_inventory_rel (inventory_id, inventory_set_id)
 SELECT i.id, invs.id FROM inventory_sets invs
 left join sets s on invs.set_id = s.id
 left join inventories_tmp i on s.set_num = i.set_num;
 
+
+-- Update inventories table (is_latest)
 
 create view v_latest_inventory as
 select i.id from (select * from inventories where set_id is not null) i
@@ -70,9 +122,11 @@ left join inventories i on i.id = mir.inventory_id
 left join inventory_minifigs im on im.id = mir.inventory_minifig_id
 group by im.fig_id) as im_max on im_max.fig_id = im.fig_id and im_max.max_version = i.version;
 
-
 UPDATE inventories SET is_latest = 1 WHERE inventories.id IN (SELECT id FROM v_latest_inventory);
 UPDATE inventories SET is_latest = 0 WHERE inventories.id NOT IN (SELECT id FROM v_latest_inventory);
+
+
+-- Update part_color_frequencies table (quantity)
 
 create view v_total_quantities as
 select part_id, color_id, sum(quantity) as quantity
